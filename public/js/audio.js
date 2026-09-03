97fccd994a5a120fd12e4b81dda6c089ab84e8f0
Catmint <wtfgayerlittlezooid@gmail.com>
2026-09-02 23:35:58 -0500

    First Commit

--- public/js/audio.js
+++ public/js/audio.js
@@ -2,7 +2,7 @@
 
 function play_plushie_sfx() {
     // Storing SFX
-    var floatieSFX = new Audio("/assets/audio/sfx/magolor_chat.mp3");
+    var floatieSFX = new Audio("/public/assets/audio/sfx/magolor_chat.mp3");
     // Lowering Volume
     floatieSFX.volume = 0.2;
     // Playing sound
